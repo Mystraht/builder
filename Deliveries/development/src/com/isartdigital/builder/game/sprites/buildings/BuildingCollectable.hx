fa2d175baf4cbf1f6d52f6814f972496c1854442
Dorian Milliere <migra87@gmail.com>
2016-01-22 22:37:26 +0100

    wip

---
+++ Deliveries/development/src/com/isartdigital/builder/game/sprites/buildings/BuildingCollectable.hx
@@ -0,0 +1,19 @@
+package com.isartdigital.builder.game.sprites.buildings;
+
+/**
+ * ...
+ * @author Dorian MILLIERE
+ */
+class BuildingCollectable
+{
+
+	public function new() 
+	{
+		
+	}
+	
+	
+	public function collect():Void {
+		trace('collecté');
+	}
+}
---
+++ Deliveries/development/src/com/isartdigital/builder/game/sprites/buildings/BuildingUpgradable.hx
@@ -0,0 +1,15 @@
+package com.isartdigital.builder.game.sprites.buildings;
+
+/**
+ * ...
+ * @author Dorian MILLIERE
+ */
+class BuildingUpgradable
+{
+
+	public function new() 
+	{
+		
+	}
+	
+}
